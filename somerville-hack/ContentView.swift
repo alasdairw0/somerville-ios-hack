6914272ba2012f62560d17e54d2d1cfab8e05071
Alasdair West <alasdair.west@and.digital>
2021-03-04 09:32:15 +0000

    Initial Commit

---
+++ somerville-hack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  somerville-hack
+//
+//  Created by Alasdair West on 04/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ somerville-hack/somerville_hackApp.swift
@@ -0,0 +1,17 @@
+//
+//  somerville_hackApp.swift
+//  somerville-hack
+//
+//  Created by Alasdair West on 04/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct somerville_hackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
