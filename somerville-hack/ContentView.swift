5aaa600f86eeaf8cc93fc00c4023d6c93ad41b67
Alasdair West <alasdair.west@and.digital>
2021-03-04 10:00:10 +0000

    Initial commit

--- somerville-hack/ContentView.swift
+++ somerville-hack/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            Text("Hello, world!")
+                .padding()
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                .shadow(radius: /*@START_MENU_TOKEN@*/24/*@END_MENU_TOKEN@*/)
+            Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Button")/*@END_MENU_TOKEN@*/
+            }
+        }
     }
 }
 
